ec090ffb1b70179b8ce4dd5819d33314df278e50
Alexander <formysite.ru@yandex.ru>
2023-04-16 10:21:20 +0300

    start

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Задачки по PHP от простого к сложному</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>
+            Задачки по РНР от простого к сложному
+        </h1>
+    </header>
+    <section class="begin">
+        <div class="">
+            <p>
+                Здесь мы будем решать задачки по РНР от простого к сложному.
+            </p>
+            <p>
+                Мы будем не только решать задачки, но и писать код к решению этих задачек.<br>
+                Начнем с простых и далее будем переходить к более сложным.
+            </p>
+            <p>
+                Ко всем задачам будет будет код решения, но не ко всем задачам он будет виден через браузер.<br>
+                К некоторым задачам будет написано только решение, но без кода или как правильно решить задачу.
+            </p>
+        </div>
+    </section>
+    <section class=""></section>
+    <section class=""></section>
+    <section class=""></section>
+    <footer></footer>
+</body>
+
+</html>
